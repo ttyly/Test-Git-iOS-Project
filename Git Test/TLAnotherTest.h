40e410bc909991de60de9d33efdc6cbf486d8ef2
Ty Ly <ttyly@yahoo.com>
2014-05-14 10:58:49 -0700

    added a test class

---
+++ Git Test/TLAnotherTest.h
@@ -0,0 +1,13 @@
+//
+//  TLAnotherTest.h
+//  Git Test
+//
+//  Created by Ty Ly-Tran on 5/14/14.
+//  Copyright (c) 2014 Ty Ly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TLAnotherTest : NSObject
+
+@end
---
+++ Git Test/TLAnotherTest.m
@@ -0,0 +1,13 @@
+//
+//  TLAnotherTest.m
+//  Git Test
+//
+//  Created by Ty Ly-Tran on 5/14/14.
+//  Copyright (c) 2014 Ty Ly. All rights reserved.
+//
+
+#import "TLAnotherTest.h"
+
+@implementation TLAnotherTest
+
+@end
