6d94061298dc75ac6e1bb980c3359e1924832925
Ty Ly <ttyly@yahoo.com>
2014-05-14 10:49:11 -0700

    first commit

---
+++ Git Test/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  Git Test
+//
+//  Created by Ty Ly-Tran on 5/14/14.
+//  Copyright (c) 2014 Ty Ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/TLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLViewController.h
+//  Git Test
+//
+//  Created by Ty Ly-Tran on 5/14/14.
+//  Copyright (c) 2014 Ty Ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLViewController : UIViewController
+
+@end
